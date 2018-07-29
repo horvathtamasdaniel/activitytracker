391835a3ba18ec87f687936135566d9fc305aff1
Benjámin Barócsi <beno922@gmail.com>
2018-07-29 12:44:14 +0200

    Track class ready, tests running.

--- src/main/java/activity/Track.java
+++ src/main/java/activity/Track.java
@@ -7,6 +7,10 @@ public class Track {
 
     private List<TrackPoint> trackPoints = new ArrayList<>();
 
+    public List<TrackPoint> getTrackPoints() {
+        return trackPoints;
+    }
+
     public void addTrackPoint(TrackPoint trackPoint) {
         if (trackPoint == null) {
             throw new IllegalArgumentException("Trackpoint must not be null!");
@@ -14,27 +18,70 @@ public class Track {
         trackPoints.add(trackPoint);
     }
 
-    public Coordinate findMaximumCoordinate() {
-        if (!isValidList(trackPoints)) {
+    public double getDistance() {
+        if (isInvalidList(trackPoints)) {
             throw new IllegalArgumentException("List must not be null or empty!");
-    }
-    double maxLat = trackPoints.get(0).getCoordinate().getLatitude();
-
-    double maxLong = trackPoints.get(0).getCoordinate().getLongitude();
-
-    for (TrackPoint tp: trackPoints) {
-        if (tp.getCoordinate().getLatitude() > maxLat) {
-            maxLat = tp.getCoordinate().getLatitude();
         }
-        if (tp.getCoordinate().getLongitude() > maxLong) {
-            maxLong = tp.getCoordinate().getLongitude();
+        if (trackPoints.size() == 1) {
+            return 0.0;
         }
+        double sum = 0.0;
+
+        for (int i = 0; i < (trackPoints.size() - 1); i++) {
+            sum += trackPoints.get(i).getDistanceFrom(trackPoints.get(i + 1));
+        }
+        return sum;
     }
+
+    public double getFullDecrease() {
+        if (isInvalidList(trackPoints)) {
+            throw new IllegalArgumentException("List must not be null or empty!");
+        }
+        double sum = 0.0;
+
+        for (int i = 0; i < (trackPoints.size() - 1); i++) {
+            if (trackPoints.get(i + 1).getElevation() < trackPoints.get(i).getElevation()) {
+                sum += (trackPoints.get(i).getElevation() - trackPoints.get(i + 1).getElevation());
+            }
+        }
+        return sum;
+    }
+
+    public double getFullElevation() {
+        if (isInvalidList(trackPoints)) {
+            throw new IllegalArgumentException("List must not be null or empty!");
+        }
+        double sum = 0.0;
+
+        for (int i = 0; i < (trackPoints.size() - 1); i++) {
+            if (trackPoints.get(i + 1).getElevation() > trackPoints.get(i).getElevation()) {
+                sum += (trackPoints.get(i + 1).getElevation() - trackPoints.get(i).getElevation());
+            }
+        }
+        return sum;
+    }
+
+    public Coordinate findMaximumCoordinate() {
+        if (isInvalidList(trackPoints)) {
+            throw new IllegalArgumentException("List must not be null or empty!");
+        }
+        double maxLat = trackPoints.get(0).getCoordinate().getLatitude();
+
+        double maxLong = trackPoints.get(0).getCoordinate().getLongitude();
+
+        for (TrackPoint tp: trackPoints) {
+            if (tp.getCoordinate().getLatitude() > maxLat) {
+                maxLat = tp.getCoordinate().getLatitude();
+            }
+            if (tp.getCoordinate().getLongitude() > maxLong) {
+                maxLong = tp.getCoordinate().getLongitude();
+            }
+        }
         return new Coordinate(maxLat, maxLong);
     }
 
     public Coordinate findMinimumCoordinate() {
-        if (!isValidList(trackPoints)) {
+        if (isInvalidList(trackPoints)) {
             throw new IllegalArgumentException("List must not be null or empty!");
         }
         double minLat = trackPoints.get(0).getCoordinate().getLatitude();
@@ -52,65 +99,18 @@ public class Track {
         return new Coordinate(minLat, minLong);
     }
 
-    public double getDistance() {
-        if (!isValidList(trackPoints)) {
-            throw new IllegalArgumentException("List must not be null or empty!");
-        }
-        if (trackPoints.size() == 1) {
-            return 0;
-        }
-        double sum = 0;
-
-        for (int i = 0; i < (trackPoints.size() - 1); i++) {
-            sum += trackPoints.get(i).getDistanceFrom(trackPoints.get(i + 1));
-        }
-        return sum;
-    }
-
-    public double getFullDecrease() {
-        if (!isValidList(trackPoints)) {
-            throw new IllegalArgumentException("List must not be null or empty!");
-        }
-        double sum = 0;
-
-        for (int i = 0; i < (trackPoints.size() - 1); i++) {
-            if (trackPoints.get(i + 1).getElevation() < trackPoints.get(i).getElevation()) {
-                sum+= (trackPoints.get(i).getElevation() - trackPoints.get(i + 1).getElevation());
-            }
-        }
-        return sum;
-    }
-
-    public double getFullElevation() {
-        if (!isValidList(trackPoints)) {
-            throw new IllegalArgumentException("List must not be null or empty!");
-        }
-        double sum = 0;
-
-        for (int i = 0; i < (trackPoints.size() - 1); i++) {
-            if (trackPoints.get(i + 1).getElevation() > trackPoints.get(i).getElevation()) {
-                sum+= (trackPoints.get(i + 1).getElevation() - trackPoints.get(i).getElevation());
-            }
-        }
-        return sum;
-    }
-
     public double getRectangleArea() {
         double a = (findMaximumCoordinate().getLatitude() - findMinimumCoordinate().getLatitude());
 
         double b = (findMaximumCoordinate().getLongitude() - findMinimumCoordinate().getLongitude());
 
-        return a * b;
+        return (a * b);
     }
 
-    public List<TrackPoint> getTrackPoints() {
-        return trackPoints;
-    }
-
-    private boolean isValidList(List list) {
+    private boolean isInvalidList(List list) {
         if ((list == null) || (list.size() == 0)) {
-            return false;
+            return true;
         }
-        return true;
+        return false;
     }
 }
